6edc485d954a9984a5b02239090b413721397074
Quentin Fallito <quentinfallito@gmail.com>
2025-11-04 14:10:14 +0100

    test led driver bourrin

--- led_driver/main.c
+++ led_driver/main.c
@@ -11,23 +11,7 @@
 void spi_init(){
     DDRB |= (1 << PB3) | (1 << PB5); //set MOSI and SCK as Output
     DDRC |= (1 << PC1) | (1 << PC2); //set LE and OE as Output
-    SPCR =  (1 << SPE) | (1 << MSTR) | (1 << SPR0); //enable spi, set as master, set clock rate fck/16
-}
-void spi_send(uint8_t data){
-    SPDR = data; //load data for transmission
-    while(!(SPSR & (1 << SPIF))); //wait for transmission complete
-}
-void spi_send_big(uint16_t data){
-    spi_send((data >> 8) & 0xFF); //send high first
-    spi_send(data & 0xFF);        //send low last
-}
-
-// data de la forme led15_led14_led13_led12_led11_led10_led9_led8_led7_led6_led5_led4_led3_led2_led1_led0
-void send_data(uint16_t data) {
-    spi_send_big(data); //fonction send 16 bits data
-    PORTC |= (1 << PC2); //latch enable
-    PORTC &= ~(1 << PC2); //disable latch
-    
+    //SPCR =  (1 << SPE) | (1 << MSTR) | (1 << SPR0); //enable spi, set as master, set clock rate fck/16 (useless for our purpose)
 }
 
 void leds_on(){
@@ -37,9 +21,30 @@ void leds_off(){
     PORTC |= (1 << PC1); //OE high
 }
 
-void display(uint16_t data){
+
+void data_send_bourrin(uint16_t data){
+    for (int i = 0; i < 16; i++)
+    {
+        if (data & (1<<(16-i)) == 1)
+        {
+            DDRB |= (1<<PB3); //set MOSI a 1
+            DDRB |= (1 << PB5); //clk rise
+        }
+        else 
+        {
+            DDRB &= ~(1<<PB3); //set MOSI a 0
+            DDRB |= (1 << PB5); //clk rise
+        }
+        DDRB &= ~(1 << PB5); //clk fall
+        DDRB &= ~(1<<PB3); // MOSI a 0 parce que je suis parano    
+    }
+    PORTC |= (1 << PC2); //latch enable
+    PORTC &= ~(1 << PC2); //disable latch
+}
+
+void display_bourrin(uint16_t data){
     leds_off();
-    send_data(data);
+    data_send_bourrin(data);
     leds_on();
 }
 
@@ -48,14 +53,37 @@ uint16_t B = 0b0101010101010101;
 
 int main(void) {
     spi_init();
-
     while (1)
     {
-            display(A);
-            _delay_ms(1000);
-            display(B);
-            _delay_ms(1000);
+        display_bourrin(A);
+        _delay_ms(1000);
+        display_bourrin(B);
+        _delay_ms(1000);
     }
+
     return(0);
     
 }
+
+
+
+// void spi_send(uint8_t data){
+//     SPDR = data; //load data for transmission
+//     while(!(SPSR & (1 << SPIF))); //wait for transmission complete
+// }
+// void spi_send_big(uint16_t data){
+//     spi_send((data >> 8) & 0xFF); //send high first
+//     spi_send(data & 0xFF);        //send low last
+// }
+// // data de la forme led15_led14_led13_led12_led11_led10_led9_led8_led7_led6_led5_led4_led3_led2_led1_led0
+// void send_data(uint16_t data) {
+//     spi_send_big(data); //fonction send 16 bits data
+
+//     PORTC |= (1 << PC2); //latch enable
+//     PORTC &= ~(1 << PC2); //disable latch
+// }
+// void display(uint16_t data){
+//     leds_off();
+//     send_data(data);
+//     leds_on();
+// }
